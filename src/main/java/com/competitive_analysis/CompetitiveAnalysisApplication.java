88bd73081435802038191326774d6712cac77ce1
Umesh Tiwari <klau.tiwari1234@gmail.com>
2025-07-21 09:29:57 +0530

    code updated

--- src/main/java/com/competitive_analysis/CompetitiveAnalysisApplication.java
+++ src/main/java/com/competitive_analysis/CompetitiveAnalysisApplication.java
@@ -2,8 +2,17 @@ package com.competitive_analysis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				DataSourceAutoConfiguration.class,
+				DataSourceTransactionManagerAutoConfiguration.class,
+				HibernateJpaAutoConfiguration.class
+		}
+)
 public class CompetitiveAnalysisApplication {
 
 	public static void main(String[] args) {
